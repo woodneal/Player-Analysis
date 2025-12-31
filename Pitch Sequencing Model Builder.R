505100ca357318cf75aad58be53abee9cd0ea07f
Neal Wood <131564263+woodneal@users.noreply.github.com>
2025-12-31 17:43:15 -0500

    Add files via upload

---
+++ Pitch Sequencing Model Builder.R
@@ -0,0 +1,20 @@
+# ============================================================
+# Predictive Modeling - Pitch Sequencing
+# Project Start - November 16, 2025
+# Last Updated: 11/16/25
+# ============================================================
+
+# ------------------------------------------------------------------------------
+# Load Libraries
+# ------------------------------------------------------------------------------
+
+install.packages("caret")
+install.packages("glmnet")
+install.packages("randomForest")
+
+library(dplyr)
+library(caret)
+library(glmnet)
+library(randomForest)
+
+
